fb0fa341ff4046d1e2c01ee847302388cbe32516
Tobias Prisching <tobias.prisching@icloud.com>
2023-07-17 16:32:40 +0200

    Started work on p5

---
+++ p_5/src/main.rs
@@ -0,0 +1,28 @@
+fn 
+main() 
+{
+    println!("Hello, world!");
+}
+
+/// Computes the prime decomposition of val
+fn
+prime_decomp
+(
+	val: u64
+)
+-> Vec<u64>
+{
+	
+	todo!()
+}
+
+/// Computes primes smaller than val
+fn
+primes_until
+(
+	val: u64
+)
+-> Vec<u64>
+{
+	todo!()
+}
