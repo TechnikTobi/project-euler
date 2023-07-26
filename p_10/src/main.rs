b0ad201ba38f4573a024fd5061fe1079aa34e0ef
Tobias Prisching <tobias.prisching@icloud.com>
2023-07-27 00:17:58 +0200

    Solved Problem 10
    142913828922

--- p_10/src/main.rs
+++ p_10/src/main.rs
@@ -1,3 +1,49 @@
-fn main() {
-    println!("Hello, world!");
+fn 
+main() 
+{
+	println!("Hello, world! {:?}", create_primes_until(100));
+	println!("{}", create_primes_until(2000000).iter().sum::<u64>());
+}
+
+
+fn
+create_primes_until
+(
+	limit: u64
+)
+-> Vec<u64>
+{
+	let mut primes = vec![2, 3, 5];
+	let mut candidate = 7;
+
+	while primes.last().unwrap() < &limit
+	{
+		let mut is_prime = true;
+		for prime in &primes
+		{
+			// if *prime as f64 > (candidate as f64).sqrt()
+			// {
+			// 	break;
+			// }
+
+			if candidate % prime == 0
+			{
+				is_prime = false;
+				break;
+			}
+			
+		}
+
+		if is_prime
+		{
+			println!("{}", candidate);
+			primes.push(candidate)
+		}
+
+		candidate += 2;
+	}
+	
+	// Remove last prime that exceeds the limit
+	primes.pop();
+	return primes;
 }
