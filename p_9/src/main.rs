d34a7596c133ec08585d7843c775a012bb5647af
Tobias Prisching <tobias.prisching@icloud.com>
2023-07-26 16:38:26 +0200

    Solved Problem 9
    31875000

--- p_9/src/main.rs
+++ p_9/src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-	println!("Hello, world!");
+fn 
+main() 
+{
+	let (a, b, c) = find_pythagorean_triplet(1000, 1000, 1000, 1000);
+	println!("{} * {} * {} = {}", a, b, c, a*b*c);
+}
+
+fn
+find_pythagorean_triplet
+(
+	sum: u64,
+	max_a: u64,
+	max_b: u64,
+	max_c: u64,
+)
+-> (u64, u64, u64)
+{
+	for a in 1..max_a
+	{
+		for b in a..max_b
+		{
+			for c in b..max_c
+			{
+				if a*a + b*b != c*c
+				{
+					continue;
+				}
+
+				if a + b + c != sum
+				{
+					continue;
+				}
+
+				return (a, b, c);
+			}
+		}
+	}
+
+	return (0, 0, 0);
 }
