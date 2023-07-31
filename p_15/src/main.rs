f5487b705a168e65829c7d6534fd8726f5c5d5f4
Tobias Prisching <tobias.prisching@icloud.com>
2023-07-31 15:41:05 +0200

    Working on P15...

--- p_15/src/main.rs
+++ p_15/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+fn 
+main() 
+{
+	println!("Hello, world! {}", count_moves(3,3));
+}
+
+fn
+count_moves
+(
+	avail_right: usize,
+	avail_down:  usize,
+)
+-> u64
+{
+	if avail_down == 0 || avail_right == 0
+	{
+		return 1;
+	}
+
+	// return count_moves(avail_right, avail_down-1) + count_moves(avail_right-1, avail_down);
+	return 
+		2 * count_moves(avail_right-1, avail_down-1) + 
+		2 * 
 }
