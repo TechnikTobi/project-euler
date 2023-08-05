2e43ccb542b784b689f0046b67d667e5fdd3b82b
Tobias Prisching <tobias.prisching@icloud.com>
2023-08-05 11:20:32 +0200

    Solved P21
    31626

--- p_21/src/main.rs
+++ p_21/src/main.rs
@@ -1,3 +1,66 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashSet;
+
+fn 
+main() 
+{
+	let mut amicable_numbers = HashSet::new();
+	for number in 1..10000
+	{
+		if amicable_numbers.contains(&number)
+		{
+			continue;
+		}
+
+		if let Some(partner) = has_amicable_partner(number)
+		{
+			amicable_numbers.insert(number);
+			amicable_numbers.insert(partner);
+		}
+	}
+
+	println!("{:?}", amicable_numbers);
+	println!("{}", amicable_numbers.iter().sum::<u64>());
+}
+
+fn
+has_amicable_partner
+(
+	number: u64
+)
+-> Option<u64>
+{
+	let divisors = get_divisors(number);
+	let partner = divisors.iter().sum::<u64>();
+
+	if partner == number
+	{
+		return None;
+	}
+
+	let partner_divisors = get_divisors(partner);
+	if partner_divisors.iter().sum::<u64>() == number
+	{
+		return Some(partner);
+	}
+
+	return None;
+}
+
+fn
+get_divisors
+(
+	number: u64
+)
+-> Vec<u64>
+{
+	let mut divisors = vec![1];
+	for divisor in 2..=(number/2+1)
+	{
+		if number % divisor == 0
+		{
+			divisors.push(divisor);
+		}
+	}
+
+	return divisors;
 }
