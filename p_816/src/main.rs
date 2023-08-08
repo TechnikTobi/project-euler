d38c7aac8e1b83db849fca6fcbe0b526b86052f7
Tobias Prisching <tobias.prisching@icloud.com>
2023-08-08 23:30:22 +0200

    Implemented random generator

--- p_816/src/main.rs
+++ p_816/src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+fn 
+main() 
+{
+	println!("Hello, world!");
+	let mut gen = RandomGenerator::new();
+
+	for i in 0..10
+	{
+		println!("{}", gen.next());
+	}
+}
+
+struct
+RandomGenerator
+{
+	state: u64
+}
+
+impl
+RandomGenerator
+{
+	fn
+	new
+	()
+	-> RandomGenerator
+	{
+		RandomGenerator
+		{
+			state: 290797
+		}
+	}
+
+	fn
+	next
+	(
+		&mut self
+	)
+	-> u64
+	{
+		let result = self.state;
+		self.state = (self.state * self.state) % 50515093;
+		return result;
+	}
 }
